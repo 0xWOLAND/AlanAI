3101dd6fdb9502be7aa5bd49ec96b24bbf999755
bhargavannem <bhargav.annem@gmail.com>
2020-07-12 17:39:24 -0500

    alan sdk

---
+++ AlanAI/MapViewController.swift
@@ -0,0 +1,31 @@
+//
+//  MapViewController.swift
+//  AlanAI
+//
+//  Created by Bhargav Annem on 7/12/20.
+//  Copyright © 2020 Bhargav Annem. All rights reserved.
+//
+
+import UIKit
+import AlanSDK
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
