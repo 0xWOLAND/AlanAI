02cc80c8d70dbddbb9595072faae257d2d29636b
bhargavannem <bhargav.annem@gmail.com>
2020-07-13 13:54:35 -0500

    alan stuff

--- AlanAI/MapViewController.swift
+++ AlanAI/MapViewController.swift
@@ -11,8 +11,19 @@ import AlanSDK
 
 class MapViewController: UIViewController {
 
+    fileprivate var button: AlanButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        let config = AlanConfig(key: "0e70fc9d2f8f363a10c1d65269c59beb2e956eca572e1d8b807a3e2338fdd0dc/stage")
+        self.button = AlanButton(config: config)
+        self.button.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(self.button)
+        let b = NSLayoutConstraint(item: self.button, attribute: .bottom, relatedBy: .equal, toItem: self.view, attribute: .bottom, multiplier: 1, constant: -40)
+        let r = NSLayoutConstraint(item: self.button, attribute: .right, relatedBy: .equal, toItem: self.view, attribute: .right, multiplier: 1, constant: -20)
+        let w = NSLayoutConstraint(item: self.button, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: 64)
+        let h = NSLayoutConstraint(item: self.button, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: 64)
+        self.view.addConstraints([b, r, w, h])
 
         // Do any additional setup after loading the view.
     }
